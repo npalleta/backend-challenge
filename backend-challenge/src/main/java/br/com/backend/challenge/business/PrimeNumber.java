308e890866e69e7a957499462dcf11bd07115c55
npalleta <palleta.sp@gmail.com>
2024-04-20 21:11:22 -0300

    PrimeNumber class has been included

---
+++ backend-challenge/src/main/java/br/com/backend/challenge/business/PrimeNumber.java
@@ -0,0 +1,24 @@
+package br.com.backend.challenge.business;
+
+import java.util.stream.IntStream;
+
+public class PrimeNumber {
+
+    public boolean calculatePrimeNumber(int number) {
+        //
+        int $number = number;
+        //
+        boolean checkPrimeNum = isGreaterThanOne(number);
+        number = $number / 2;
+        //
+        if (checkPrimeNum || number <= 1)
+            return IntStream.rangeClosed(2, number).noneMatch(i -> $number % i == 0);
+        //
+        return true;
+    }
+
+    private boolean isGreaterThanOne(int number) {
+        if (number > 1) return true;
+        else throw new ArithmeticException("It's Wrong! This is a negative number, zero or one!");
+    }
+}
