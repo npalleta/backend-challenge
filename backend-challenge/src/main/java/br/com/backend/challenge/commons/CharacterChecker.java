489395f2c45eff4526d4ccb94d7c11a9bc3911b3
npalleta <palleta.sp@gmail.com>
2024-04-21 16:31:40 -0300

    fix: CharacterChecker has been changed - Before - DigitChecker

--- backend-challenge/src/main/java/br/com/backend/challenge/commons/CharacterChecker.java
+++ backend-challenge/src/main/java/br/com/backend/challenge/commons/CharacterChecker.java
@@ -2,9 +2,9 @@ package br.com.backend.challenge.commons;
 
 import java.util.regex.Pattern;
 
-public class DigitChecker {
+public class CharacterChecker {
 
-    public boolean hasOnlyLetterAndSymbols(String stringWithNumber) {
+    public boolean hasOnlyLettersAndSymbols(String stringWithNumber) {
         if (stringWithNumber.isEmpty() && stringWithNumber.isBlank())
             return false;
         return Pattern.compile(
