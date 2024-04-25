1569e9e955e6eb26e6a992bc1ae89d5f752deadd
npalleta <palleta.sp@gmail.com>
2024-04-25 15:53:59 -0300

    Adding new api with params

--- backend-challenge/src/main/java/br/com/backend/challenge/controller/JwtValidationController.java
+++ backend-challenge/src/main/java/br/com/backend/challenge/controller/JwtValidationController.java
@@ -7,6 +7,7 @@ import jakarta.validation.Valid;
 import jakarta.ws.rs.Consumes;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
 import jakarta.ws.rs.core.MediaType;
 import jakarta.ws.rs.core.Response;
 import lombok.extern.slf4j.Slf4j;
@@ -40,11 +41,41 @@ public class JwtValidationController {
             log.error("O JWT Web Token foi definido de forma inválida na requisição.");
         }
         return Response
-            .status(Response.Status.INTERNAL_SERVER_ERROR)
-            .entity("Verifique o corpo da requisição - token inválido ou inexistente.")
-            .status(Response.Status.BAD_REQUEST)
-            .entity("O JWT Web Token foi definido de forma inválida na requisição.")
-            .build();
+                .status(Response.Status.INTERNAL_SERVER_ERROR)
+                .entity("Verifique o corpo da requisição - token inválido ou inexistente.")
+                .status(Response.Status.BAD_REQUEST)
+                .entity("O JWT Web Token foi definido de forma inválida na requisição.")
+                .build();
         //
+    }
+
+    @POST
+    @Operation(summary = "Realiza a checagem do token JWT - passagem por paramêtro.")
+    @Path("/jwt-validate/{jwtPayloadToken}")
+    public Response getJwtDecoded(@PathParam("jwtPayloadToken") @Valid String jwtPayloadToken) {
+        try {
+            log.info("-- Validando um token JWT proprietário --");
+            if (jwtPayloadToken != null && !jwtPayloadToken.isEmpty()) {
+                String decodedToken = this.jwtValidationService.validateJwtPayload(jwtPayloadToken);
+                String result = "{\"result\": \"" + decodedToken + "\"}";
+
+                return Response.status(Response.Status.CREATED)
+                    .entity(result)
+                    .build();
+                //
+            } else {
+                log.error("O JWT Web Token não foi fornecido na requisição.");
+                return Response.status(Response.Status.BAD_REQUEST)
+                    .entity("O JWT Web Token não foi fornecido na requisição.")
+                    .build();
+                //
+            }
+        } catch (RuntimeException ex) {
+            log.error("Ocorreu um erro ao validar o token JWT.");
+            return Response.status(Response.Status.INTERNAL_SERVER_ERROR)
+                .entity("Ocorreu um erro ao validar o token JWT.")
+                .build();
+            //
+        }
     }
 }
